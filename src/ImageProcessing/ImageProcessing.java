f54e30677236c3d6d8f628e8d1ea17fd4c9d580a
Dita Maheswari <ditamaheswari05@gmail.com>
2025-04-02 15:58:13 +0700

    update ImageProcessing.java

--- src/ImageProcessing/ImageProcessing.java
+++ src/ImageProcessing/ImageProcessing.java
@@ -2,45 +2,62 @@ package ImageProcessing;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
-public class ImageProcessing{
-    private  Color[][] matriksImage;
+public class ImageProcessing {
+    private Color[][] matriksImage;
     private int width;
     private int height;
 
-    // Constructor
-    public ImageProcessing(BufferedImage image){
+    // Constructor untuk seluruh gambar
+    public ImageProcessing(BufferedImage image) {
         this.width = image.getWidth();
         this.height = image.getHeight();
         matriksImage = new Color[height][width];
 
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width; x++){
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 matriksImage[y][x] = new Color(image.getRGB(x, y));
             }
         }
     }
 
+    // Constructor untuk region gambar
+    public ImageProcessing(BufferedImage image, int startX, int startY, int regionWidth, int regionHeight) {
+        this.width = Math.min(regionWidth, image.getWidth() - startX);
+        this.height = Math.min(regionHeight, image.getHeight() - startY);
+        matriksImage = new Color[height][width];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (startX + x < image.getWidth() && startY + y < image.getHeight()) {
+                    matriksImage[y][x] = new Color(image.getRGB(startX + x, startY + y));
+                } else {
+                    matriksImage[y][x] = Color.BLACK; // Default jika di luar gambar
+                }
+            }
+        }
+    }
+
     // Getter
-    public Color getPixel(int y, int x){
+    public Color getPixel(int y, int x) {
         return matriksImage[y][x];
     }
 
-    public int getHeight(){
+    public int getHeight() {
         return height;
     }
 
-    public int getWidth(){
+    public int getWidth() {
         return width;
     }
 
-    public Color avgColor(int startX, int startY, int blockWidth, int blockHeight){
+    public Color avgColor(int startX, int startY, int blockWidth, int blockHeight) {
         long sumR = 0;
         long sumG = 0;
         long sumB = 0;
 
         int ctr = 0;
-        for (int y = startY; y < startY + blockHeight && y < height; y++){
-            for (int x = startX; x < startX + blockWidth && x < width; x++){
+        for (int y = startY; y < startY + blockHeight && y < height; y++) {
+            for (int x = startX; x < startX + blockWidth && x < width; x++) {
                 Color c = matriksImage[y][x];
                 sumR += c.getRed();
                 sumG += c.getGreen();
@@ -49,7 +66,7 @@ public class ImageProcessing{
             }
         }
 
-        if (ctr == 0){
+        if (ctr == 0) {
             return new Color(0, 0, 0);
         }
 
